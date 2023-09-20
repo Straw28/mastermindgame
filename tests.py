14e9e44c2d20d90951b55e606df76611d0b134bf
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-09-19 21:50:30 -0500

    changes to test.py

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from src.main import ColorCode, Feedback
+from src.main import Feedback
 
 #Canary Test
 class TestCanary(unittest.TestCase):
@@ -11,12 +11,11 @@ class TestCanary(unittest.TestCase):
 #No Black or Silver
 class ComIncorrect(unittest.TestCase):
     def test_incorrect_guess(self):
-        color_code = ColorCode()
         feedback = Feedback()
 
-        color_code.game_color_code = [1,2,3,4,5,6]
+        game_color_code = [1,2,3,4,5,6]
         user_guess = [6,5,4,3,2,1]
-        result = feedback.get_result_of_guess(user_guess, color_code.game_color_code)
+        result = feedback.get_result_of_guess(user_guess, game_color_code)
 
         self.assertNotIn(1, result)
     
