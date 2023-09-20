9a70aabacd84f5feacf39f3ada77e3950a435db4
Ly Ha <lythituyetha@gmail.com>
2023-09-19 22:15:15 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -13,13 +13,35 @@ class AllIncorrect(unittest.TestCase):
     def test_incorrect_guess(self):
         feedback = Feedback()
 
-        game_color_code = [1,2,3,4,5,6]
-        user_guess = [7,8,9,10,11,12]
-        result = feedback.get_result_of_guess(user_guess, game_color_code)
+        actual_color_sequence = [1,2,3,4,5,6]
+        user_guess_sequence = [7,8,9,10,11,12]
+        result = feedback.get_result_of_guess(user_guess_sequence, actual_color_sequence)
 
         self.assertNotIn(1, result)
         self.assertNotIn(0, result)
     
+class IncorrectBlack(unittest.TestCase):
+    def test_incorrect_black_count(self):
+        
 
+        user_guess_sequence = [1,2,3,4,5,6]
+        actual_color_sequence = [1,2,3,4,7,8]
+
+        result = self.Feedback.get_result_of_guess(user_guess_sequence,actual_color_sequence)
+
+        self.assertEqual(result.count(1),2)
+
+
+class IncorrectSilver(unittest.TestCase):
+    def test_incorrect_silver_count(self):
+        
+
+        user_guess_sequence = [1,2,3,4,5,6]
+        actual_color_sequence = [6,5,4,3,2,1]
+
+        result = self.Feedback.get_result_of_guess(user_guess_sequence,actual_color_sequence)
+
+        self.assertEqual(result.count(0),3)
+    
 if  __name__ == '__main__':
     unittest.main()
