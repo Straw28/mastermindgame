e6284452000194d76fcba893d4db5fabb15a2170
Ly Ha <lythituyetha@gmail.com>
2023-09-19 22:05:08 -0500

    first 2 tests

--- tests.py
+++ tests.py
@@ -9,15 +9,16 @@ class TestCanary(unittest.TestCase):
    
 
 #No Black or Silver
-class ComIncorrect(unittest.TestCase):
+class AllIncorrect(unittest.TestCase):
     def test_incorrect_guess(self):
         feedback = Feedback()
 
         game_color_code = [1,2,3,4,5,6]
-        user_guess = [6,5,4,3,2,1]
+        user_guess = [7,8,9,10,11,12]
         result = feedback.get_result_of_guess(user_guess, game_color_code)
 
         self.assertNotIn(1, result)
+        self.assertNotIn(0, result)
     
 
 if  __name__ == '__main__':
