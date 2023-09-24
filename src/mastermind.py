43ee90a3bbe2c89e1bfa5c1c080a5e07440ee044
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-09-23 19:49:10 -0500

    updated test case

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,15 +1,15 @@
 class MasterMind:
-  def code_match(self, user_guess, actual_color_code): 
-    def get_result_of_guess(self, user_guess_sequence, actual_color_sequence):
-        correct_guesses = [-1] * len(actual_color_sequence) #initially we'll set everything to 0 (silver) and assume incorrect till we compare
-        for i, guess in enumerate(actual_color_sequence):
-            if int(user_guess_sequence[i]) == actual_color_sequence[i]:
-                correct_guesses[i] = 1 #black = 1
-            elif int(user_guess_sequence[i]) in actual_color_sequence:
-                correct_guesses[i] = 0
-        return correct_guesses
-    
-    return False
+  def code_match(self, user_guess, actual_color_code):
+
+      correct_guesses = [''] * len(actual_color_code) 
+      
+      for i, guess in enumerate(actual_color_code):
+          if int(user_guess[i]) == actual_color_code[i]:
+              correct_guesses[i] = 'black'
+          elif int(user_guess[i]) in actual_color_code:
+              correct_guesses[i] = 'silver'
+      
+      return correct_guesses
 
 
 
---
+++ src/test_mastermind.py
@@ -0,0 +1,20 @@
+import unittest
+#from src.mastermind import MasterMind
+from mastermind import MasterMind
+
+class MasterMindTests(unittest.TestCase):
+  def test_canary(self):
+    self.assertTrue(True)
+
+  def test_code_match_results_in_matching_code(self):
+    user_guess = ['orange','blue ','green','yellow','red','pink']
+    actual_code = ['orange','blue ','green','yellow','red','pink']
+
+    response = MasterMind().code_match(user_guess, actual_code)
+    expected = ['black'] * 6
+    
+    self.assertEqual(expected, response)
+
+if __name__ == '__main__': 
+  unittest.main()
+
