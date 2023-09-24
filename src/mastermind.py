28048d92e9d22dc7fe2c03e891dcc0715fd93151
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-09-23 19:24:13 -0500

    old feedback

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,7 +1,13 @@
 class MasterMind:
-  def code_match(self, user_guess, actual_color_code):
-    if user_guess == actual_color_code:
-      return True
+  def code_match(self, user_guess, actual_color_code): 
+    def get_result_of_guess(self, user_guess_sequence, actual_color_sequence):
+        correct_guesses = [-1] * len(actual_color_sequence) #initially we'll set everything to 0 (silver) and assume incorrect till we compare
+        for i, guess in enumerate(actual_color_sequence):
+            if int(user_guess_sequence[i]) == actual_color_sequence[i]:
+                correct_guesses[i] = 1 #black = 1
+            elif int(user_guess_sequence[i]) in actual_color_sequence:
+                correct_guesses[i] = 0
+        return correct_guesses
     
     return False
 
