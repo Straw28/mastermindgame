5f6bf5f07e2f0691e56b662233beac34c7b51dd6
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-09-27 15:49:35 -0500

    upding

---
+++ src/test_mastermind.py
@@ -0,0 +1,39 @@
+import unittest
+from parameterized import parameterized
+# from src.mastermind import guess
+# from src.mastermind import Colors
+# from src.mastermind import Match
+from mastermind import guess
+from mastermind import Colors
+from mastermind import Match
+
+
+globals().update( Colors.__members__)
+globals().update( Match.__members__)
+
+class MasterMindTests(unittest.TestCase):
+  def test_canary(self):
+    self.assertTrue(True)
+
+
+@parameterized.expand(
+    [
+      ([YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [EXACT] * 6),
+            ([YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [PINK, YELLOW, RED, GREEN, ORANGE, CYAN], [PARTIAL] * 6),
+            ([YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [VIOLET, BLUE, TEAL, MAGENTA, BROWN, SKY_BLUE], [UNKNOWN] * 6),
+            ([YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [YELLOW, RED, GREEN, ORANGE, BROWN, SKY_BLUE], [EXACT, EXACT, EXACT, EXACT, UNKNOWN, UNKNOWN]),
+            ([YELLOW, RED, GREEN, ORANGE, CYAN, PINK], [VIOLET, BLUE, GREEN, ORANGE, CYAN, PINK], [EXACT, EXACT, EXACT, EXACT, UNKNOWN, UNKNOWN]),
+            ([YELLOW, RED, GREEN, CYAN, PINK, ORANGE], [YELLOW, RED, GREEN, PINK, ORANGE, CYAN], [EXACT, EXACT, EXACT, PARTIAL, PARTIAL, PARTIAL]),
+            ([YELLOW, RED, MAGENTA, CYAN, PINK, ORANGE], [BROWN, RED, GREEN, PINK, ORANGE, CYAN], [UNKNOWN, EXACT, UNKNOWN, PARTIAL, PARTIAL, PARTIAL]),
+    ],
+)
+
+def test_guess(self, selected_colors, user_provided_colors, expected_response):
+  response = guess(selected_colors, user_provided_colors)
+
+  self.assertEqual(expected_response, response)
+
+
+if __name__ == '__main__': 
+  unittest.main()
+
